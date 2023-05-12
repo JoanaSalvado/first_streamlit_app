fb7becba78025f4327d38a4a328ebce3f80d5bf6
Joana Salvado <joana.salvado@yahoo.com>
2023-05-12 16:39:46 +0100

    Update streamlit_app.py
    add paragraphs

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,4 +2,4 @@ import streamlit as st
 
 st.title('My Parents New Healthy Diner')
 st.header('Breakfast Menu')
-st.text('Omega 3 & Blueberry Oatmeal\nKale, Spinach & Rocket Smoothie\nHard-Boiled Free-Range Egg')
+st.text('Omega 3 & Blueberry Oatmeal\n\nKale, Spinach & Rocket Smoothie\n\nHard-Boiled Free-Range Egg')
