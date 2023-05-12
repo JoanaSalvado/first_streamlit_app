a93a9756346fec97ae6c6ca7e5ee986592ce8b90
Joana Salvado <joana.salvado@yahoo.com>
2023-05-12 17:01:53 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,5 @@
 import streamlit as st
+import pandas as pd
 
 st.title("My Mom's New Healthy Diner")
 st.header('Breakfast Favorites')
@@ -7,3 +8,6 @@ st.markdown(':green_salad: Kale, Spinach & Rocket Smoothie')
 st.markdown(':chicken: Hard-Boiled Free-Range Egg')
 st.markdown(':avocado::bread: Avocado Toast')
 st.header(':banana::strawberry: Build Your Own Fruit Smoothie :kiwifruit::grapes:')
+
+my_fruit_list = pd.read_csv('https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt')
+st.dataframe(my_fruit_list)
