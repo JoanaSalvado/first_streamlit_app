79e76aaa9add1c34a1c17caba271f8bc15a73eb9
Joana Salvado <joana.salvado@yahoo.com>
2023-05-12 16:56:25 +0100

    Update streamlit_app.py
    Correct emoji call

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,8 +1,8 @@
 import streamlit as st
 
-st.title('My Parents New Healthy Diner')
-st.header('Breakfast Menu')
-st.text(':bowl_with_spoon:vOmega 3 & Blueberry Oatmeal')
-st.text(':green_salad: Kale, Spinach & Rocket Smoothie')
-st.text(':chicken: Hard-Boiled Free-Range Egg')
-st.text(':avocado::bread: Avocado Toast')
+st.title("My Mom's New Healthy Diner")
+st.header('Breakfast Favorites')
+st.markdown(':bowl_with_spoon: Omega 3 & Blueberry Oatmeal')
+st.markdown(':green_salad: Kale, Spinach & Rocket Smoothie')
+st.markdown(':chicken: Hard-Boiled Free-Range Egg')
+st.markdown(':avocado::bread: Avocado Toast')
