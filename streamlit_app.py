d4849dbe349bbc681de2b3d1a02da2382fc7e5db
Joana Salvado <joana.salvado@yahoo.com>
2023-05-12 16:38:36 +0100

    Update streamlit_app.py
    Add Breakfast Menu

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,5 @@
-import streamlit
+import streamlit as st
 
-streamlit.title('My Parents New Healthy Diner')
+st.title('My Parents New Healthy Diner')
+st.header('Breakfast Menu')
+st.text('Omega 3 & Blueberry Oatmeal\nKale, Spinach & Rocket Smoothie\nHard-Boiled Free-Range Egg')
