dbcc2423ec6b0fb23d0c40317d7a7e13240ad3fe
Joana Salvado <joana.salvado@yahoo.com>
2023-05-15 16:35:43 +0100

    Update streamlit_app.py
    add snowflake connector prompt

--- streamlit_app.py
+++ streamlit_app.py
@@ -33,3 +33,10 @@ fruityvice_response = rq.get("https://fruityvice.com/api/fruit/" + fruit_choice)
 # Make the Fruityvice data looking nice
 fruityvice_normalized = pd.json_normalize(fruityvice_response.json())
 st.dataframe(fruityvice_normalized)
+
+my_cnx = snowflake.connector.connect(**st.secrets["snowflake"])
+my_cur = my_cnx.cursor()
+my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()")
+my_data_row = my_cur.fetchone()
+st.text("Hello from Snowflake:")
+st.text(my_data_row)
