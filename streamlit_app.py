5e1e478d0d374d31cfe844ac5d4714c85e6b1145
Joana Salvado <joana.salvado@yahoo.com>
2023-05-15 16:53:03 +0100

    Update streamlit_app.py
    add try-except to fruitvice table

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,6 +2,7 @@ import streamlit as st
 import pandas as pd
 import requests as rq
 import snowflake.connector
+from urllib.error import URLError
 
 st.title("My Mom's New Healthy Diner")
 st.header('Breakfast Favorites')
@@ -25,15 +26,21 @@ st.dataframe(fruits_to_show)
 # Get Fruityvice API data
 st.header('Fruityvice Fruit Advice!')
 
-fruit_choice = st.text_input('What fruit would you like information about?', 'Kiwi')
-st.write('The user entered', fruit_choice)
-
-fruityvice_response = rq.get("https://fruityvice.com/api/fruit/" + fruit_choice)
+try:
+  fruit_choice = st.text_input('What fruit would you like information about?', 'Kiwi')
+  if not fruit_choice:
+     st.error('Please select a fruit to get information.')
+  else:
+    fruityvice_response = rq.get("https://fruityvice.com/api/fruit/" + fruit_choice)
+    fruityvice_normalized = pd.json_normalize(fruityvice_response.json())
+    st.dataframe(fruityvice_normalized)
+ 
+except:
+  st.error()
 
 # Make the Fruityvice data looking nice
-fruityvice_normalized = pd.json_normalize(fruityvice_response.json())
-st.dataframe(fruityvice_normalized)
 
+st.stop()
 my_cnx = snowflake.connector.connect(**st.secrets["snowflake"])
 my_cur = my_cnx.cursor()
 my_cur.execute("SELECT * FROM FRUIT_LOAD_LIST")
